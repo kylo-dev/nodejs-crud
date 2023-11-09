0a0c3b33f2012f51fd30081210513762fa0e07f6
hyungyeom <rlagusrua3687@gmail.com>
2023-11-09 15:25:07 +0900

    feat : boardtype create & create_process 구현

--- gcshop/lib/board.js
+++ gcshop/lib/board.js
@@ -45,5 +45,50 @@ module.exports = {
             });
         });
     },
+    typecreate : (req, res)=>{
+        if(!checkSessionClass(req, res)){
+            return;
+        }
+        db.query('select * from boardtype', (err, boardtypes)=>{
+            var context = {
+                menu: 'menuForManager.ejs',
+                who: req.session.name,
+                logined: 'YES',
+                boardtypes: boardtypes,
+                body: 'boardtypeCU.ejs',
+                check: 'c'
+            };
+    
+            req.app.render('home', context, (err, html)=>{
+                res.end(html);
+            });
+        });
+    },
+    typecreate_process : (req, res)=>{
+        var post = req.body;
+        title = sanitizeHtml(post.title);
+        description = sanitizeHtml(post.description);
+        numPerPage = sanitizeHtml(post.numPerPage);
+        writeYN = sanitizeHtml(post.write_YN);
+        reYN = sanitizeHtml(post.re_YN);
+
+        db.query(`insert into boardtype(title,description, write_YN, re_YN, numPerPage) values(?,?,?,?,?)`,
+            [title, description, writeYN, reYN, numPerPage], (err, result)=>{
+                if(err){
+                    throw err;
+                }
+                res.writeHead(302, {Location: `/board/type/view`});
+                res.end();
+            });
+    },
+    typeupdate : (req, res)=>{
+
+    },
+    typeupdate_process: (req, res)=>{
+
+    },
+    typedelete_process : (req, res)=>{
+
+    }
 
 }
